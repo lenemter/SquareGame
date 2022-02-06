f79b8abba6f89f0fa9d0bbfa5d93fb72ef1fa29a
lenemter <lenemter@gmail.com>
2022-02-06 21:29:14 +0900

    Camera tracking Fix player movement

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ class Player(pygame.sprite.Sprite):
         self.w = 0.5
         self.h = 0.5
         self.rect = pygame.Rect(0, 0, self.w * BLOCK_SIZE_X, self.h * BLOCK_SIZE_Y)
-
+        
+        # Camera
         self.last_camera_dx = 0
         self.last_camera_dy = 0
 
@@ -30,16 +31,24 @@ class Player(pygame.sprite.Sprite):
         self.rect.y = self.y * BLOCK_SIZE_Y + dy
         pygame.draw.rect(screen, PLAYER_COLOR, self.rect, 0)
 
-    def event_handler(self, event):
+    def event_handler(self, event, time):
         keys = pygame.key.get_pressed()
         dx = (
-            max(keys[pygame.K_RIGHT], keys[pygame.K_d])
-            - max(keys[pygame.K_LEFT], keys[pygame.K_a])
-        ) * SPEED
+            (
+                max(keys[pygame.K_RIGHT], keys[pygame.K_d])
+                - max(keys[pygame.K_LEFT], keys[pygame.K_a])
+            )
+            * SPEED
+            * time
+        )
         dy = (
-            max(keys[pygame.K_DOWN], keys[pygame.K_s])
-            - max(keys[pygame.K_UP], keys[pygame.K_w])
-        ) * SPEED
+            (
+                max(keys[pygame.K_DOWN], keys[pygame.K_s])
+                - max(keys[pygame.K_UP], keys[pygame.K_w])
+            )
+            * SPEED
+            * time
+        )
 
         if dx != 0:
             self.move_single_axis(dx, 0)
@@ -47,32 +56,43 @@ class Player(pygame.sprite.Sprite):
             self.move_single_axis(0, dy)
 
     def move_single_axis(self, dx, dy):
-        self.x += dx
-        self.rect.x += dx * BLOCK_SIZE_X
-        self.y += dy
-        self.rect.y += dy * BLOCK_SIZE_Y
+        last_rect_x = self.rect.x
+        last_rect_y = self.rect.y
 
-        for wall in walls_group:
-            if self.rect.colliderect(wall.rect):
-                if dx > 0:  # Moving right; Hit the left side of the wall
-                    self.x = wall.x - self.w
-                    self.rect.x = self.x * BLOCK_SIZE_X + self.last_camera_dx
-                    break
-                if dx < 0:  # Moving left; Hit the right side of the wall
-                    self.x = wall.x + wall.w
-                    self.rect.x = self.x * BLOCK_SIZE_X + self.last_camera_dx
-                    break
-                if dy > 0:  # Moving down; Hit the top side of the wall
-                    # print("COLIDE")
-                    self.y = wall.y - self.h
-                    self.rect.y = self.y * BLOCK_SIZE_Y + self.last_camera_dy
-                    break
-                if dy < 0:  # Moving up; Hit the bottom side of the wall
-                    self.y = wall.y + wall.h
-                    self.rect.y = self.y * BLOCK_SIZE_Y + self.last_camera_dy
-                    break
-        self.rect.x = self.x * BLOCK_SIZE_X + self.last_camera_dx
-        self.rect.y = self.y * BLOCK_SIZE_Y + self.last_camera_dy
+        self.x += dx
+        self.rect.x = int(self.x * BLOCK_SIZE_X + self.last_camera_dx)
+        if self.rect.x == last_rect_x and dx != 0:
+            self.x -= dx
+            self.rect.x = last_rect_x
+            return None
+        
+        self.y += dy
+        self.rect.y = int(self.y * BLOCK_SIZE_Y + self.last_camera_dy)
+        if self.rect.y == last_rect_y and dy != 0:
+            self.y -= dy
+            self.rect.y = last_rect_y
+            return None
+
+        # logging.debug(self.x, self.y)
+
+        walls_hits = pygame.sprite.spritecollide(self, walls_group, False)
+        for wall in walls_hits:
+            if dx > 0:  # Moving right; Hit the left side of the wall
+                self.x = wall.x - self.w
+                self.rect.x = last_rect_x
+                break
+            elif dx < 0:  # Moving left; Hit the right side of the wall
+                self.x = wall.x + wall.w
+                self.rect.x = last_rect_x
+                break
+            elif dy > 0:  # Moving down; Hit the top side of the wall
+                self.y = wall.y - self.h
+                self.rect.y = last_rect_y
+                break
+            elif dy < 0:  # Moving up; Hit the bottom side of the wall
+                self.y = wall.y + wall.h
+                self.rect.y = last_rect_y
+                break
 
 
 class Wall(pygame.sprite.Sprite):
@@ -99,15 +119,15 @@ class Camera:
         screen.fill(BACKGROUND_COLOR)
         # logging.debug(f"CAM: {self.x=} {self.y=}")
         for obj in all_group:
-            # logging.debug(f"OBJ: {obj.x=} {obj.y=}")
             obj.draw(self.x, self.y)
 
     def update(self, target):
-        self.x = target.x + self.x + target.w // 2 - WINDOW_SIZE[0] // 2
-        self.y = target.y + self.y + target.h // 2 - WINDOW_SIZE[1] // 2
+        print(self.x, self.y)
+        self.x = target.rect.w // 2 - target.x * BLOCK_SIZE_X + WINDOW_SIZE_X_2
+        self.y = target.rect.h // 2 - target.y * BLOCK_SIZE_Y + WINDOW_SIZE_Y_2
 
 
-class Menu:
+class TestLevel:
     def __init__(self):
         self.player = None
         self.level = []
@@ -115,7 +135,7 @@ class Menu:
         self.load_map()
 
     def load_map(self):
-        with open("levels/menu.txt", mode="r", encoding="UTF-8") as file:
+        with open("levels/test_level.txt", mode="r", encoding="UTF-8") as file:
             level = file.readlines()
         for y in range(len(level)):
             self.level.append([])
@@ -128,20 +148,20 @@ class Menu:
                 elif level[y][x] == "@":
                     self.player = Player(x, y)
 
-    def event_handler(self, event):
-        # self.camera.update(self.player)  # PROBLEM HERE
-        self.player.event_handler(event)
+    def event_handler(self, event, time):
+        self.player.event_handler(event, time)
+        self.camera.update(self.player)
         self.camera.draw()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     pygame.init()
-    screen = pygame.display.set_mode(WINDOW_SIZE, pygame.SCALED)
+    screen = pygame.display.set_mode((WINDOW_SIZE_X, WINDOW_SIZE_Y))
     screen.fill(BACKGROUND_COLOR)
-    pygame.display.set_caption("<Название>")
+    pygame.display.set_caption(WINDOW_NAME)
 
-    menu = Menu()
+    level = TestLevel()
     clock = pygame.time.Clock()
     running = True
 
@@ -149,8 +169,7 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        clock.tick(FPS)
-        menu.event_handler(event)
+        level.event_handler(event, clock.tick(FPS))
         pygame.display.flip()
 
     pygame.quit()
