5bdd63f4d016ea42b4d3424aebcfad9e06531e1f
lenemter <lenemter@gmail.com>
2022-02-07 16:55:57 +0900

    No level generator Health system

--- utils.py
+++ utils.py
@@ -1,16 +1,21 @@
 import pygame
 
 
+# Window
 WINDOW_SIZE_X, WINDOW_SIZE_Y = (1280, 720)
 WINDOW_SIZE_X_2, WINDOW_SIZE_Y_2 = (WINDOW_SIZE_X // 2, WINDOW_SIZE_Y // 2)
 WINDOW_NAME = "<Название>"
+
+# Game
+TEST_LEVEL = "levels/test_level.txt"
 BLOCK_SIZE_X, BLOCK_SIZE_Y = (50, 50)
-SPEED = 0.005
 FPS = 120
 
-# FONT = pygame.font.SysFont("Impact", 20)
+# Player
+SPEED = 0.005
+HEALTH_LIMIT = 3
 
-
+# Colors
 WHITE = pygame.color.Color("white")
 PLAYER_COLOR = pygame.color.Color("blue")
 BACKGROUND_COLOR = pygame.color.Color("#333333")
