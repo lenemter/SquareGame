13cf59d7051b66188dedb0faac395717d099f31c
lenemter <lenemter@gmail.com>
2022-02-08 23:04:00 +0900

    Refactoring

--- menu.py
+++ menu.py
@@ -1,93 +1,101 @@
 import pygame
 
-from common import *
+from common import (
+    WINDOW_SIZE_X,
+    WINDOW_SIZE_Y,
+    WINDOW_NAME,
+    BUTTON_COLOR,
+    BUTTON_HIGHLIGHT_COLOR,
+    WHITE,
+    BACKGROUND_COLOR,
+    BUTTON_SIZE_X,
+    BUTTON_SIZE_Y,
+)
 
 
-class Button:
-    def __init__(self, screen, coordinates, dimensions, text=""):
-        self.screen = screen
+class Button(pygame.sprite.Sprite):
+    def __init__(self, group, coordinates, dimensions, text=""):
+        super().__init__(group)
         self.color = BUTTON_COLOR
         self.x, self.y = coordinates[0], coordinates[1]
         self.width, self.height = dimensions[0], dimensions[1]
         self.text = text
 
-    def draw(self):
+    def draw(self, surface):
         pygame.draw.rect(
-            self.screen, self.color, (self.x, self.y, self.width, self.height), 0
+            surface, self.color, (self.x, self.y, self.width, self.height), 0
         )
 
         if self.text != "":
             font = pygame.font.SysFont("ComicSans", 32)
             text = font.render(self.text, True, WHITE)
-            screen.blit(
+            surface.blit(
                 text,
                 (
-                    self.x + (self.width / 2 - text.get_width() / 2),
-                    self.y + (self.height / 2 - text.get_height() / 2),
+                    self.x + (self.width - text.get_width()) // 2,
+                    self.y + (self.height - text.get_height()) // 2,
                 ),
             )
 
-    def hover(self):
+    def is_hovered(self):
         mouse_position = pygame.mouse.get_pos()
-        if self.x < mouse_position[0] < self.x + self.width:
-            if self.y < mouse_position[1] < self.y + self.height:
-                return True
-        return False
+        return (
+            self.x < mouse_position[0] < self.x + self.width
+            and self.y < mouse_position[1] < self.y + self.height
+        )
+
+    def event_handler(self, events, events_types):
+        if self.is_hovered():
+            self.color = BUTTON_HIGHLIGHT_COLOR
+        else:
+            self.color = BUTTON_COLOR
+            return None
+
+        if pygame.MOUSEBUTTONDOWN in events_types:
+            print(f'Кнопка "{self.text}" нажата')
 
 
-if __name__ == "__main__":
+def main():
     pygame.init()
     pygame.display.set_caption(WINDOW_NAME)
 
     screen = pygame.display.set_mode((WINDOW_SIZE_X, WINDOW_SIZE_Y))
     screen.fill(BACKGROUND_COLOR)
-    clock = pygame.time.Clock()
 
     x_pos = (screen.get_width() - BUTTON_SIZE_X) / 2
 
-    play_button = Button(screen, (x_pos, 100), (BUTTON_SIZE_X, BUTTON_SIZE_Y), "Играть")
+    buttons_group = pygame.sprite.Group()
+
+    play_button = Button(
+        buttons_group, (x_pos, 100), (BUTTON_SIZE_X, BUTTON_SIZE_Y), "Играть"
+    )
     collections_button = Button(
-        screen, (x_pos, 175), (BUTTON_SIZE_X, BUTTON_SIZE_Y), "Коллекции"
+        buttons_group, (x_pos, 175), (BUTTON_SIZE_X, BUTTON_SIZE_Y), "Коллекции"
     )
     stats_button = Button(
-        screen, (x_pos, 250), (BUTTON_SIZE_X, BUTTON_SIZE_Y), "Статистика"
+        buttons_group, (x_pos, 250), (BUTTON_SIZE_X, BUTTON_SIZE_Y), "Статистика"
     )
 
     running = True
 
     while running:
-        play_button.draw()
-        collections_button.draw()
-        stats_button.draw()
+        play_button.draw(screen)
+        collections_button.draw(screen)
+        stats_button.draw(screen)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+        events = pygame.event.get()
+        events_types = {event.type for event in events}
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if play_button.hover():
-                    print("Кнопка Играть нажата")
-                if collections_button.hover():
-                    print("Кнопка Коллекции нажата")
-                if stats_button.hover():
-                    print("Кнопка Статистика нажата")
+        if pygame.QUIT in events_types:
+            running = False
 
-            if event.type == pygame.MOUSEMOTION:
-                if play_button.hover():
-                    play_button.color = BUTTON_HIGHLIGHT_COLOR
-                else:
-                    play_button.color = BUTTON_COLOR
-
-                if collections_button.hover():
-                    collections_button.color = BUTTON_HIGHLIGHT_COLOR
-                else:
-                    collections_button.color = BUTTON_COLOR
-
-                if stats_button.hover():
-                    stats_button.color = BUTTON_HIGHLIGHT_COLOR
-                else:
-                    stats_button.color = BUTTON_COLOR
+        for button in buttons_group:
+            button.event_handler(events, events_types)
 
         pygame.display.flip()
 
     pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
